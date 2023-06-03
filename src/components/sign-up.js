0f07d714a71089ead1e81b707ac7ab1925888bf0
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-03 14:16:53 +0100

    password matching function

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -5,11 +5,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
 const PWD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 const SignUp = () => {
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, setUser] = useState("")
+    const [validName, setValidName] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
+
+    const [pwd, setPwd] = useState("")
+    const [validPwd, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
+
+    const [matchPwd, setMatchPwd] = useState("")
+    const [validMatch, setValidMatch] = useState(false)
+    const [MatchFocus, setMatchFocus] = useState(false)
+
+    const [errMessage, setErrMessage] = useState('')
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focus()
+    },[])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user)
+        console.log(result)
+        console.log(user)
+        setValidName(result)
+    }, [user])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd)
+        console.log(result)
+        console.log(pwd)
+        setValidPwd(result)
+        const match = pwd === matchPwd
+        setValidMatch(match)
+    }, [pwd, matchPwd]
+        )
 
     return (
-        <>
-            <h1>sign up</h1>
-        </>
+        <div>
+            
+        </div>
     )
 }
 
