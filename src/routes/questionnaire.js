80708079a6c1cdff0df7a7a0b21c5759efe8b651
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-05 17:07:01 +0100

    questionnaire finished

--- src/routes/questionnaire.js
+++ src/routes/questionnaire.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import apiRoute from '../lib/apiRoute';
 import authAxios from '../lib/authAxios';
 const Questionnaire = () => {
-    const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answer, setAnswer] = useState();
   useEffect(() => {
@@ -25,33 +25,41 @@ const Questionnaire = () => {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } 
   };
-
+  console.log(currentQuestionIndex + 1)
+  console.log(answer)
   const saveAnswer = async () => {
-  const response = await authAxios.post(`${apiRoute}api/v1/answers`, {question_id: currentQuestionIndex +1, answer: answer});
+    const response = await authAxios.post(`${apiRoute}api/v1/answers`, { question_id: currentQuestionIndex + 1, answer: answer });
     console.log(response)
+    setAnswer("")
   };
 
-const currentQuestion = questions[currentQuestionIndex];
+  const currentQuestion = questions[currentQuestionIndex];
+  
+  if (currentQuestionIndex < questions.length) {
+    return (
+    
+      <div className='py-16 px-32 bg-black-transparent rounded-xl'>
+        {currentQuestion && (
+          <div className='w-screen'>
+            <h3 className='forum text-6xl'>Question {currentQuestionIndex + 1}</h3>
+            <h4 className='forum text-3xl mt-8'>{currentQuestion.category}</h4>
+            <p className='forum text-2xl mt-8'>{currentQuestion.text}</p>
+            <textarea
+              className='bg-black-transparent2 border border-white rounded-sm w-9/12 h-64 mont mt-4 focus:outline-none box-shadow p-4'
+              value={answer}
+              onChange={handleAnswerChange}
+            />
+          </div>
+        )}
+        <button className='button mt-8' onClick={(e) => { e.preventDefault(); saveAnswer(); handleNextQuestion() }}>
+          {currentQuestionIndex < questions.length - 1 ? 'Next' : 'Finish'}
+        </button>
+      </div>);
+  } else {
+    
 
-  return (
-    <div className='py-16 px-32 bg-black-transparent rounded-xl'>
-      {currentQuestion && (
-        <div className='w-screen'>
-                <h3 className='forum text-6xl'>Question {currentQuestionIndex + 1}</h3>
-                <h4 className='forum text-3xl mt-8'>{currentQuestion.category}</h4>
-                <p className='forum text-2xl mt-8'>{currentQuestion.text}</p>
-          <textarea
-            className='bg-black-transparent2 border border-white rounded-sm w-9/12 h-64 mont mt-4 focus:outline-none box-shadow p-4'
-            value={answer}
-            onChange={handleAnswerChange}
-          />
-        </div>
-      )}
-      <button className='button mt-8' onClick={(e) => { e.preventDefault(); saveAnswer(); handleNextQuestion() }}>
-        {currentQuestionIndex < questions.length - 1 ? 'Next' : 'Submit'}
-      </button>
-    </div>
-  );
-};
+  }
+
+}
 
 export default Questionnaire;
